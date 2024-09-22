97c145ef90d68bc5ee9765e4649111e300abab2a
jabss <jabrijuhinin93@gmail.com>
2024-09-22 14:21:52 +0800

    install vite

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1> Workout Tracker !</h1>
+      <button onClick={()=> console.log('clicked')}> CLICK </button>
     </div>
   );
 }
