f76ca4c6248583a82697a1acc34d5cabe5e0f08e
jabss <jabrijuhinin93@gmail.com>
2024-10-16 00:05:51 +0400

    empty input box after adding all workout info

--- src/WoTrack.jsx
+++ src/WoTrack.jsx
@@ -51,6 +51,8 @@ function App() {
           setWorkout([...workout, response.data])
         })
       setNewWorkout('')
+      setNewWorkoutDate('')
+      setNewWorkoutDetail('')
     }
 
   const handleLike = (id) => {
