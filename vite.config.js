6a65f23fe3e7ed94d9d3923c4b93babdd00f2083
jabss <jabrijuhinin93@gmail.com>
2024-10-16 21:21:27 +0400

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,  // Automatically opens the browser
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3003',
+        changeOrigin: true,
+      },
+    }
   },
 });
