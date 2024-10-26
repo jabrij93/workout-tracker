abff2b3274d8da8a5fa94b6536a218cfd73eede3
jabss <jabrijuhinin93@gmail.com>
2024-10-26 16:18:32 +0400

    text paragraph color changes to 'green' or 'red' as according to 'success' and 'error'

--- components/Notification.jsx
+++ components/Notification.jsx
@@ -1,9 +1,11 @@
 export const Notification = ({ notification, type }) => {
     const borderColor = type === 'error' ? 'red' : 'green'
+    const textColor = type === 'error' ? 'red' : 'green'
+    
 
     return (
         <div className="notification" style={{ border:`2px solid ${borderColor}`, borderRadius:'6px' }}>
-            <p > { notification } </p>
+            <p style={{ color: textColor}}> { notification } </p>
         </div>
     )
 }
