22e07c28a94843dceb85766018b61973643329db
jabss <jabrijuhinin93@gmail.com>
2024-10-26 13:21:49 +0400

    add border and border radius for notification component

--- components/Notification.jsx
+++ components/Notification.jsx
@@ -1,6 +1,6 @@
 export const Notification = () => {
     return (
-        <div className="notification">
+        <div className="notification" style={{ border:'2px solid black', borderRadius:'6px' }}>
             <p>TEST NOTI</p>
         </div>
     )
