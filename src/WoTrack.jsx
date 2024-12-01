23624d59e3012557effee543c574994a85cd9835
jabss <jabrijuhinin93@gmail.com>
2024-12-01 11:37:15 +0400

    remove generateId() function

--- src/WoTrack.jsx
+++ src/WoTrack.jsx
@@ -21,26 +21,6 @@ const WoTrack = ({ user, isLoggedIn, setIsLoggedIn }) => {
 
   console.log('userinWotrack', user)
 
-  // Generate id for every new data workout 
-  const generateId = () => {
-    // Generate two random lowercase letters (a-z)
-    const letter1 = String.fromCharCode(
-      Math.floor(Math.random() * 26) + 97 // First random letter
-    );
-  
-    const letter2 = String.fromCharCode(
-      Math.floor(Math.random() * 26) + 97 // First random letter
-    );
-  
-    // Generate two random digits (0-9)
-    const number1 = Math.floor(Math.random() * 9).toString()
-    const number2 = Math.floor(Math.random() * 9).toString()
-  
-    // Combine the two letters and two digits
-    return letter1 + number1 + letter2 + number2
-  };
-
-
   useEffect(() => {
     workoutService
       .getAll()
@@ -69,7 +49,6 @@ const WoTrack = ({ user, isLoggedIn, setIsLoggedIn }) => {
     const formattedDate = dayjs(newWorkoutDate).format('DD-MM-YYYY'); // Matches GridCalendar
   
     const newWorkoutData = {
-      id: generateId(),
       workouts: newWorkout,
       date: formattedDate,
       detail: newWorkoutDetail,
