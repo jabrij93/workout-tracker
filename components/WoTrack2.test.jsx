30836d017ed2558cf296338525b2a8727326981d
jabss <jabrijuhinin93@gmail.com>
2024-12-23 19:14:02 +0400

    update wotrack2 test

--- components/WoTrack2.test.jsx
+++ components/WoTrack2.test.jsx
@@ -11,8 +11,11 @@ test('renders content', () => {
     workouts: 'pull up'
   }
 
-  // Mock the axios get request
-  axios.get.mockResolvedValue({ data: [] })
+  // The event handler is a mock function defined with Vitest:
+  const createWorkout = vi.fn();
+
+  // A session is started to interact with the rendered component:
+  const user = userEvent.setup();
 
   render(<WoTrack2 createWorkout={workout} user={{ username: 'root', password: 'salainen' }} isLoggedIn={true} setIsLoggedIn={() => {}} buttonLabel="show details" />)
 
