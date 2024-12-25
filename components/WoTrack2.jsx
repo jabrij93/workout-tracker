ec5c65f1a4fa0a78fce77cd6af6f20ae4f75fb46
jabss <jabrijuhinin93@gmail.com>
2024-12-25 21:39:56 +0400

    extract 'article' into its own component(now renamed to MainWorkout.jsx)

--- components/WoTrack2.jsx
+++ components/WoTrack2.jsx
@@ -5,6 +5,7 @@ import GridCalendar from './GridCalendar.jsx';
 import { Notification } from './Notification.jsx';
 import Togglable from './Togglable.jsx';
 import WorkoutForm from "./WorkoutForm";
+import MainWorkout from "./MainWorkout"; 
 
 const WoTrack2 = ({ createWorkout, user, isLoggedIn, setIsLoggedIn, buttonLabel }) => {
   const [count, setCount] = useState(0);
@@ -357,51 +358,14 @@ const WoTrack2 = ({ createWorkout, user, isLoggedIn, setIsLoggedIn, buttonLabel 
     
       {/* Main Content */}
       <div className="main-content">
-        <div className="article">
-          <h3 className="project-header">Your Activities/Workouts</h3>
-          {Object.keys(groupedWorkouts).map((monthYear) => (
-            <div className="card-container" key={monthYear}>
-              <div>
-                <h2>{monthYear}</h2>
-                {groupedWorkouts[monthYear].map((item, index) => (
-                  <div className="card" key={index}>
-                    <div className="title">
-                      <p>{item.workouts}</p>
-                      <button onClick={() => toggleVisibility(`${monthYear}-${index}`)}>{buttonLabel}</button>
-                    </div>
-                    <div style={visible[`${monthYear}-${index}`] ? workoutContainer : showWhenVisible } className={`togglableContent ${
-                      visible[`${monthYear}-${index}`] ? "visible" : "hidden"}`}>
-                      <p className="detail">{item.date} - {item.detail}</p> 
-                        <div className="card-features">
-                          <div className="favourite">
-                            <img
-                              src="images/favouritelogo.svg"
-                              alt="Favourite"
-                              style={{ width: "18px", height: "18px" }}
-                            />
-                          </div>
-                          <div className="view">
-                            <img
-                              src="images/viewlogo.svg"
-                              alt="View"
-                              style={{ width: "18px", height: "18px" }}
-                            />
-                          </div>
-                          <div className="share">
-                            <img
-                              src="images/sharelogo.svg"
-                              alt="Share"
-                              style={{ width: "18px", height: "18px" }}
-                            />
-                          </div>
-                        </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
+        <MainWorkout
+          groupedWorkouts={groupedWorkouts}
+          toggleVisibility={toggleVisibility}
+          visible={visible}
+          workoutContainer={workoutContainer}
+          showWhenVisible={showWhenVisible}
+          buttonLabel={buttonLabel}
+        />
   
         <div className="right-container">
           <div className="announcements">
