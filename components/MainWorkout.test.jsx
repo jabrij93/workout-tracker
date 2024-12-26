ec4071a413cf7d241b789ecd8b9659d3b018380f
jabss <jabrijuhinin93@gmail.com>
2024-12-26 18:39:33 +0400

    clicking button test working

--- components/MainWorkout.test.jsx
+++ components/MainWorkout.test.jsx
@@ -7,7 +7,7 @@ test('renders workouts', async () => {
         "January 2024": [
             {
                 workouts: 'front lever',
-                date: '15-12-2024',
+                date: '17-12-2024',
                 detail: '2 x 11 scapular pull-ups, 60s deadhang 60s, 6 minutes rest',
                 likes: 0,
                 user: {
@@ -36,6 +36,48 @@ test('renders workouts', async () => {
         buttonLabel={buttonLabel}
       />)
   
-    const div = container.querySelector('.title')
-    expect(div).toHaveTextContent('front lever')
+    const div = container.querySelector('.detail')
+    screen.debug(div)
+    expect(div).toHaveTextContent('17-12-2024')
+})
+
+test('clicking the button calls event handler once', async () => {
+    const groupedWorkouts = {
+        "January 2024": [
+            {
+                workouts: 'front lever',
+                date: '17-12-2024',
+                detail: '2 x 11 scapular pull-ups, 60s deadhang 60s, 6 minutes rest',
+                likes: 0,
+                user: {
+                    username: 'root',
+                    name: 'Superuser',
+                    id: '6734a7c7be415b97d507c03a'
+                }
+            }
+        ]
+    }
+
+    const mockHandler = vi.fn()
+    const visible = vi.fn();
+    const workoutContainer = { display: 'flex', justifyContent: 'space-between', flexDirection: 'column' };
+    const showWhenVisible = { display: visible ? '' : 'none' };
+    
+    const buttonLabel = 'show details'
+  
+    render(
+      <MainWorkout 
+        groupedWorkouts={groupedWorkouts}
+        toggleVisibility={mockHandler}
+        visible={visible}
+        workoutContainer={workoutContainer}
+        showWhenVisible={showWhenVisible}
+        buttonLabel={buttonLabel}
+      />)
+  
+    const user = userEvent.setup()  
+    const button = screen.getByText('show details')
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(1)
 })
