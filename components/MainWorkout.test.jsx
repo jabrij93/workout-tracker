3cb0368d8ea0927fe271af17cdaac1631b65da90
jabss <jabrijuhinin93@gmail.com>
2024-12-30 21:15:09 +0400

    after clicking the button twice, children are displayed and close back test working

--- components/MainWorkout.test.jsx
+++ components/MainWorkout.test.jsx
@@ -172,3 +172,60 @@ test('at start the children are not displayed', () => {
        return element.textContent.includes('2 x 11 scapular pull-ups, 60s deadhang 60s, 6 minutes rest');
      }));
 })
+
+test('after clicking the button twice, children are displayed and close back', async () => {
+    const groupedWorkouts = {
+        "January 2024": [
+            {
+                workouts: 'front lever',
+                date: '17-12-2024',
+                detail: '2 x 11 scapular pull-ups, 60s deadhang 60s, 6 minutes rest',
+                likes: 0,
+                user: {
+                    username: 'root',
+                    name: 'Superuser',
+                    id: '6734a7c7be415b97d507c03a'
+                }
+            }
+        ]
+    }
+
+    // Mock visibility states
+    const visible = { "January 2024-0": false };
+    const mockHandler = vi.fn();
+    const workoutContainer = { display: 'flex', justifyContent: 'space-between', flexDirection: 'column' };
+    const showWhenVisible = { display: visible ? '' : 'none' };
+    
+    const buttonLabel = 'show details'
+  
+    render(
+      <MainWorkout 
+        groupedWorkouts={groupedWorkouts}
+        toggleVisibility={mockHandler}
+        visible={visible}
+        workoutContainer={workoutContainer}
+        showWhenVisible={showWhenVisible}
+        buttonLabel={buttonLabel}
+      />)
+  
+    const user = userEvent.setup()  
+    const button = screen.getByText('show details')
+    await user.click(button)
+
+    // Check visibility class
+    //  const div = container.querySelector('.togglableContent')
+    //  expect(div).toHaveClass('visible') // Expect class to be visible
+
+     // Check if the date and details are displayed
+     expect(screen.findAllByText((content, element) => {
+       return element.textContent.includes('17-12-2024');
+     }));
+     expect(screen.findAllByText((content, element) => {
+       return element.textContent.includes('2 x 11 scapular pull-ups, 60s deadhang 60s, 6 minutes rest');
+     }));
+
+     await user.click(button)
+
+     expect(screen.queryByText('17-12-2024')).not.toBeInTheDocument();
+    expect(screen.queryByText('2 x 11 scapular pull-ups, 60s deadhang 60s, 6 minutes rest')).not.toBeInTheDocument();
+})
