af1d487c7990ad92fba1fe0ba2952c85deb7fb66
jabss <jabrijuhinin93@gmail.com>
2025-01-31 02:36:14 +0400

    add data-testid on component

--- components/MainWorkout.jsx
+++ components/MainWorkout.jsx
@@ -8,12 +8,12 @@ const MainWorkout = ({ groupedWorkouts, toggleVisibility, visible, workoutContai
         <div className="card-container" key={monthYear}>
           <div>
             <h2>{monthYear}</h2>
-            {groupedWorkouts[monthYear].map((item, index) => {
+            {groupedWorkouts[monthYear].map((item, index) => { 
               const isVisible = visible[`${monthYear}-${index}`];
               const style = isVisible ? workoutContainer : { display: 'none' };
 
               return (
-                <div className="card" key={index}>
+                <div className="card" key={index} data-testid="workout-card">
                   <div className="title">
                     <p>{item.workouts}</p>
                     <button onClick={() => toggleVisibility(`${monthYear}-${index}`)}>
