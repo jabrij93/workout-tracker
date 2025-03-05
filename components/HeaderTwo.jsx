45b8fa592b7d9c8c10e24e8fad41094215a4977c
jabss <jabrijuhinin93@gmail.com>
2025-03-06 00:01:47 +0800

    refactor header-two into HeaderTwo

---
+++ components/HeaderTwo.jsx
@@ -0,0 +1,21 @@
+import GridCalendar from '../components/GridCalendar.jsx';
+
+const HeaderTwo = ({ notification, notificationType, totalWorkouts, openModal, calendarData }) => {
+    return (
+      <div className="header-two">
+        <div style={{ marginLeft: "15px", marginTop: "-20px", paddingBottom: "20px" }}>
+          {notification && <Notification notification={notification} type={notificationType} />}
+          
+          <div className="header">
+            <p>{totalWorkouts} Workouts/Activities in the last year</p>
+            <button className="new" onClick={openModal}>NEW +</button>  
+          </div>
+  
+          <GridCalendar calendarData={calendarData} />
+          <br /> 
+        </div>
+      </div>
+    );
+  };
+  
+export default HeaderTwo;
--- src/WoTrack2.jsx
+++ src/WoTrack2.jsx
@@ -15,6 +15,7 @@ import DashboardOne from '../components/DashboardOne.jsx';
 import DashboardTwo from '../components/DashboardTwo.jsx';
 import SearchBar from '../components/SearchBar.jsx';
 import HeaderOne from '../components/HeaderOne.jsx';
+import HeaderTwo from '../components/HeaderTwo.jsx';
 
 const WoTrack2 = ({ createWorkout, user, isLoggedIn, setIsLoggedIn, buttonLabel }) => {
   const [count, setCount] = useState(0);
@@ -245,20 +246,13 @@ const WoTrack2 = ({ createWorkout, user, isLoggedIn, setIsLoggedIn, buttonLabel 
         user={user} 
       />
 
-      <div className="header-two">
-        <div style={{ marginLeft:"15px", marginTop:"-20px", paddingBottom:"20px" }}>
-            {notification && (
-                <Notification notification={notification} type={notificationType} />
-            )}
-            <div className="header">
-              <p> {totalWorkouts} Workouts/Activities in the last year </p>
-              <button className="new" onClick={openModal}>NEW +</button>  
-            </div>
-             
-            <GridCalendar calendarData={calendarData} />
-            <br/> 
-        </div>
-      </div>
+      <HeaderTwo
+        notification={notification}
+        notificationType={notificationType}
+        totalWorkouts={totalWorkouts}
+        openModal={openModal}
+        calendarData={calendarData}
+      />
 
     
       {/* Main Content */}
