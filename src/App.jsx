70e6c4174b8b59609fad173c6fa3f1c12f94dc63
jabss <jabrijuhinin93@gmail.com>
2025-03-25 23:23:05 +0800

    Old WoTrack2.jsx is now moved to App.jsx instead of just implementing the component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,35 @@ import WoTrack2 from '../components/WoTrack2.jsx';
 import Notification from '../components/Notification.jsx';
 import loginService from './services/login.js';
 import workoutService from '../src/services/workouts.js';
+import TopLeftHeader from '../components/TopLeftHeader.jsx';
+import LeftSidebar from '../components/LeftSidebar.jsx';
+import DashboardHeader from '../components/DashboardHeader.jsx';
+import SearchBar from '../components/SearchBar.jsx';
+import DashboardOne from '../components/DashboardOne.jsx';
+import DashboardTwo from '../components/DashboardTwo.jsx';
+import HeaderOne from '../components/HeaderOne.jsx';
+import HeaderTwo from '../components/HeaderTwo.jsx';
+import Announcement from '../components/Announcement.jsx';
+import Trending from '../components/Trending.jsx';
+import Workout from '../components/Workout.jsx';
+
+
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [calendarData, setCalendarData] = useState({});
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [workouts, setWorkouts] = useState([]);
   const [groupedWorkouts, setGroupedWorkouts] = useState({});
+  const [notification, setNotification] = useState('');
+  const [notificationType, setNotificationType] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedInUser');
@@ -35,6 +55,22 @@ const App = () => {
     }
   };
 
+  // Hover over to X and Logout Button
+  const [isHovered, setIsHovered] = useState(false);
+  const [isHoveredX, setIsHoveredX] = useState(false);
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+  const handleMouseEnterX = () => setIsHoveredX(true);
+  const handleMouseLeaveX = () => setIsHoveredX(false);
+
+  const sortedWorkouts = workouts.filter((workout) => workout.user?.username === user.username)
+                .slice()
+                .sort((a, b) => {
+                  const dateA = new Date(a.date.split('-').reverse().join('-'));
+                  const dateB = new Date(b.date.split('-').reverse().join('-'));
+                  return dateB - dateA; // Sort descending
+  });
+
   const groupWorkouts = workouts.reduce((acc, workout) => {
     if (workout.date) {
       const [day, month, year] = workout.date.split('-'); // Extract day, month, year
@@ -56,6 +92,9 @@ const App = () => {
   });
   
   // Fetch workouts data by MonthYear
+
+  const totalWorkouts = sortedWorkouts.length;
+
   
 
   const addWorkout = (workoutObject) => {
@@ -70,6 +109,14 @@ const App = () => {
     event.preventDefault();
     window.localStorage.removeItem('loggedInUser');
     setUser(null);
+  };
+
+  const handleHamburgerClick = () => {
+    setIsSidebarOpen(true); // Open the sidebar
+  };
+                
+  const handleCloseClick = () => {
+    setIsSidebarOpen(false); // Close the sidebar
   };
 
   return (
@@ -87,35 +134,81 @@ const App = () => {
         fetchWorkouts={fetchWorkouts}
       />
       ) : (
-        <>
-          <WoTrack2
-            user={user}
-            workouts={workouts}
-            addWorkout={(newWorkout) => setWorkouts([...workouts, newWorkout])}
-            handleLogout={() => {
-              setIsLoggedIn(false);
-              setUser(null);
-            }}
-            setGroupedWorkouts={setGroupedWorkouts}
-          />
+        <div className="App">
+      {notification && (
+        <Notification notification={notification} type={notificationType} />
+      )}
+  
+      {/* Hamburger Button */}
+      <div className="wotrack-logo">
+          <TopLeftHeader handleHamburgerClick={handleHamburgerClick} />
+          <SearchBar />
+      </div>
 
-          {/* Iterate over grouped workouts here */}
-          <div>
-            {sortedMonths.map(monthYear => (
-              <div key={monthYear}>
-                <h3>{monthYear}</h3>
-                <ul>
-                  {groupWorkouts[monthYear].map(workout => (
-                    <li key={workout.id}>
-                      {workout.date} - {workout.workouts}: {workout.detail}
-                    </li>
-                  ))}
-                </ul>
+      <LeftSidebar
+        isModalOpen={isModalOpen}
+        createWorkout={addWorkout}
+        setNotification={setNotification}
+        setNotificationType={setNotificationType}
+        sortedWorkouts={sortedWorkouts}
+      />
+  
+      {/* Sidebar */}
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`} id="sidebar">
+            <DashboardHeader
+              handleCloseClick={handleCloseClick}
+              isHoveredX={isHoveredX}
+              handleMouseEnterX={handleMouseEnterX}
+              handleMouseLeaveX={handleMouseLeaveX}
+            />
+            <DashboardOne />
+            <DashboardTwo 
+              isHovered={isHovered} 
+              handleMouseEnter={handleMouseEnter} 
+              handleMouseLeave={handleMouseLeave} 
+              handleLogout={handleLogout} 
+            />
+      </div>
+  
+      {/* Header */}
+      <HeaderOne 
+        isLoggedIn={isLoggedIn} 
+        user={user} 
+      />
+
+      <HeaderTwo
+        notification={notification}
+        notificationType={notificationType}
+        totalWorkouts={totalWorkouts}
+        calendarData={calendarData}
+      />
+  
+      {/* Main Content */}
+      <div className="main-content">
+        <div className="article">
+            <h3 className="project-header">Your Activities/Workouts</h3>
+            {Object.keys(groupedWorkouts).map((monthYear) => (
+              <div className="card-container" key={monthYear}>
+                <div>
+                  <h2>{monthYear}</h2>
+                  <Workout
+                    groupedWorkouts={groupedWorkouts[monthYear]}
+                    toggleVisibility={toggleVisibility}
+                    visible={visible}
+                    workoutContainer={workoutContainer}
+                    buttonLabel="See more"
+                  />
+                </div>
               </div>
             ))}
           </div>
-        </>
-      )}
+  
+        <div className="right-container">
+          <Announcement />
+          <Trending />        
+        </div>
+      </div>
+    </div>)}
     </div>
   );
 };
